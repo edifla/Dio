22cd1383c8e946d7fd538a4d05eb805e8c7e056c
Rafael Albuquerque <54068660+edifla@users.noreply.github.com>
2021-09-27 08:23:52 -0300

    BootCamp de .net

---
+++ Desafios/Desafio 3-4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+    class MinhaClasse {
+        static void Main(string[] args) {
+            Console.WriteLine("Segundos: ");
+            var timeInSeconds = int.Parse(Console.ReadLine());
+
+       //continue a solucao
+            var hours = 0;
+            var minutes =  0;
+            var seconds = 0;
+            var m = 0;
+          
+          for(int i = 0; i < timeInSeconds; i++){
+          
+            seconds = seconds + 1;
+            
+            if(seconds == 60){
+              m = m + 1;
+              minutes = minutes + 1;
+              seconds = 0;
+            
+            }else if(m == 60){
+              minutes = 0;
+              hours = hours + 1;
+              m = 0;
+                
+            }else{
+              
+              continue;
+              
+            }
+              
+          }
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+        }
+    }
